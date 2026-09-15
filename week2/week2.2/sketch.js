2510d3b293cb34bec028747f7ea1bc232f72f5f6
ZiyanMa2922 <zm2922@nyu.edu>
2026-09-16 02:02:29 +0800

    week2 2.2 2.3  update

--- week2/week2.2/sketch.js
+++ week2/week2.2/sketch.js
@@ -5,42 +5,26 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
     background(220, 240, 255);
     xPos = width/2
-    yPos = width/2
+    yPos = height/2
+    console.log("xPos: " + xPos +",yPos: "+ yPos);
 }
 
 function draw() {
-    ellipse(200, 100, 50, 50);
-    rectMode(CENTER);
-    rect(120, 100, 30, 30);
-
-  push();
-    stroke(10, 160, 225);
-    strokeWeight(4);
-    quad(60, 80, 60, 260, 90, 260, 80, 80);
-  pop();
-
-  push();
-    fill(176, 224, 230);
-    stroke(255, 180, 230);
-    strokeWeight(6);
-    triangle(xPos, yPos - 100, xPos - 100, yPos + 50, xPos + 100, yPos + 50);
-  pop();
-
-  push();
-  noFill();
-  stroke(255, 105, 180);
-  strokeWeight(4);
-  arc(xPos, yPos, 300, 300, radians(45), radians(135), OPEN);
-  pop();
-
-  push();
-   stroke(500);
-   strokeWeight(30);
-   point(xPos-110, yPos-80);
+  angleMode(DEGREES);
+  rectMode(CENTER);
+  noStroke();
   
-   stroke(500);
-   strokeWeight(30);
-   point(xPos+110, yPos-80);
-  pop();
 
+  push();
+  translate(xPos, yPos);
+  rotate(45);
+  fill(random(0, 255), random(0, 255), random(0, 255));
+  rect(0, 0, 300, 300);
+  pop();
+}
+
+  function mousePressed() {
+  xPos = mouseX;
+  yPos = mouseY;
+  console.log("xPos: " + xPos + ",yPos: " + yPos);
 }
